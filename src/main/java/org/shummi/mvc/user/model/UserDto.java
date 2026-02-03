1c06e04f0c0854065c1e7cfa9ee96a7dabae4da4
Ilya Shumakov <shumakoff.ilya@yandex.ru>
2026-02-03 23:37:52 +0300

    refactor: dto;

--- src/main/java/org/shummi/mvc/user/model/UserDto.java
+++ src/main/java/org/shummi/mvc/user/model/UserDto.java
@@ -1,36 +1,25 @@
 package org.shummi.mvc.user.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Null;
 import jakarta.validation.constraints.Positive;
-import org.shummi.mvc.pet.Pet;
-import org.shummi.mvc.pet.model.PetDto;
 import org.shummi.mvc.user.User;
 
-import java.util.List;
-
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record UserDto(
-        @Null
-        Long id,
-
         @NotBlank
         String name,
 
         @Email
-        @NotBlank
         String email,
 
         @NotNull
         @Positive
-        Integer age,
-
-        @Null
-        List<PetDto> pets
+        Integer age
 ) {
     public User toEntity() {
-        List<Pet> petList = pets.stream().map(PetDto::toEntity).toList();
-        return new User(id, name, email, age, petList);
+        return new User(null, name, email, age, null);
     }
 }
