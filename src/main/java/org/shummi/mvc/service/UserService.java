da5b226687d61e068bada6098c81e5ee113b0e2a
Ilya Shumakov <shumakoff.ilya@yandex.ru>
2026-02-03 23:38:22 +0300

    feat: unique email;

--- src/main/java/org/shummi/mvc/service/UserService.java
+++ src/main/java/org/shummi/mvc/service/UserService.java
@@ -1,31 +1,52 @@
 package org.shummi.mvc.service;
 
+import org.shummi.mvc.exception.MvpException;
 import org.shummi.mvc.user.User;
 import org.shummi.mvc.user.model.UserDto;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class UserService {
-    private final Map<Long, User> users = new HashMap<>();
+    private final Map<Long, User> users;
+    private final Set<String> emails;
     private long nextId;
+
+    UserService() {
+        this.users = new HashMap<>();
+        this.emails = new HashSet<>();
+        this.nextId = 0L;
+    }
 
     public User createUser(UserDto request) {
         User user = request.toEntity();
-        user.setId(nextId++);
-        users.put(user.id(), user);
+        if (emails.contains(request.email())) {
+            throw new MvpException("Duplicate email");
+        }
+        user.setId(++nextId);
+        user.setPets(new ArrayList<>());
+        users.put(user.getId(), user);
+        emails.add(user.getEmail());
+
         return user;
     }
 
-    public User updateUser(UserDto request) {
-        Long id = request.id();
-        exists(id);
+    public User updateUser(Long id, UserDto request) {
+        User user = getUserById(id);
         User entity = request.toEntity();
+        if (!entity.getEmail().equals(user.getEmail())) {
+            throw new MvpException("Email cannot be changed");
+        }
+        entity.setId(id);
+        entity.setPets(user.getPets());
         users.put(id, entity);
 
         return entity;
@@ -41,8 +62,9 @@ public class UserService {
     }
 
     public void deleteById(Long id) {
-        Optional.ofNullable(users.remove(id))
+        User user = Optional.ofNullable(users.remove(id))
                 .orElseThrow(() -> new NoSuchElementException("User Not Found ID: " + id));
+        emails.remove(user.getEmail());
     }
 
     public void exists(Long id) {
